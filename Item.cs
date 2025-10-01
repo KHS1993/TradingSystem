6603be02bb5c1e13feb89e679bd2f6d803b193c0
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-01 11:44:11 +0200

    Jag har skapat namespace MyTradingApp för bättre struktur. Därefter skapade jag en beskrivning av vad jag vill att item ska visa. Jag lagrade namn och pris.  Jag skapade en lista med objekt (items) som innehåller värde (namn och pris). Jag la till flera olika saker i listan. Med en foreach-loop vill jag att den ska gå igenom listan och srkiva ut sakerna med pris.

--- Item.cs
+++ Item.cs
@@ -1,1 +1,33 @@
-// Name and description
+using System; //  behövs för console.writeline
+using System.Collections.Generic; // nödvändig för att skriva List
+
+namespace MyTradingApp
+{
+  public class Item
+  {
+    public string name;
+    public int price;
+  }
+
+  class Program
+  {
+    static void Main() // Metoden ska inte returnera ett värde utan bara kör det som är under
+    {
+      List<Item> items = new List<Item>(); // Vill skapa en lista med items och värde 
+
+      items.Add(new Item() { name = "sofa", price = 1000 });
+      items.Add(new Item() { name = "chair", price = 200 });
+      items.Add(new Item() { name = "lamp", price = 400 });
+      items.Add(new Item() { name = "clock", price = 700 });
+      items.Add(new Item() { name = "table", price = 1200 });
+
+      foreach (var item in items) // Ska skriva ut allt ovanför 
+      {
+        Console.WriteLine("Name: " + item.name + ", Price: " + item.price); // skriver ut pris och namn på varan
+      }
+
+    }
+
+  }
+
+}
