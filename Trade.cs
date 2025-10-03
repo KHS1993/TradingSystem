c0e166a9a9faf5a8f9d48c5b1ef0674f2d59ff69
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-03 15:33:23 +0200

    Add trader class with private username and password properties.

--- Trade.cs
+++ Trade.cs
@@ -1,1 +1,16 @@
 // requesters - Receiver - status - items 
+
+using System;
+
+public class Trader // Skapar en klass som ska representera en användare (Trader). 
+
+{
+
+  private string Username { get; set; } // Private på båda då endast klassen själv ska kunna ändra för ökad säkerhet. 
+  private string Password { get; set; } // get och set för att värdet ska kunna ändras men endast inifrån klassen.
+
+}
+
+
+
+
