3968daf195b18d824c96656a37976c2433d96c37
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-04 19:03:57 +0200

    Added a constructor in Item.cs in order to add new objects with name, price and description.

--- Item.cs
+++ Item.cs
@@ -3,31 +3,41 @@ using System.Collections.Generic; // nödvändig för att skriva List
 
 namespace MyTradingApp
 {
-  public class Item // Tillgängligt för andra filer genom att göra klassen public
+  public class Item  // Tillgängligt för andra filer genom att göra klassen public
   {
     public string name;
     public int price;
-  }
 
-  class Program // klassmetoden innehåller startmetoden Main, C# letar efter Main metoden för att köra programmet och det måste alltid ligga i en klass. 
-  {
-    static void Main() // Metoden ska inte returnera ett värde utan bara kör det som är under
+    public string description;
+  
+   public item(string name, int price, string description) // ska skapa nytt item när dessa tre värden skrivs in.
     {
-      List<Item> items = new List<Item>(); // Vill skapa en lista med items och värde 
+      Name = name;
+      price = price;
+      description = description;
 
-      items.Add(new Item() { name = "sofa", price = 1000 }); // skapar en ny item med namn och pris
-      items.Add(new Item() { name = "chair", price = 200 });
-      items.Add(new Item() { name = "lamp", price = 400 });
-      items.Add(new Item() { name = "clock", price = 750 });
-      items.Add(new Item() { name = "table", price = 1200 });
+    }
+  }
+ 
+  class Program // klassmetoden innehåller startmetoden Main, C# letar efter Main metoden för att köra programmet och det måste alltid ligga i en klass. 
+{
+  static void Main() // Metoden ska inte returnera ett värde utan bara kör det som är under
+  {
+    List<Item> items = new List<Item>(); // Vill skapa en lista med items och värde 
 
-      foreach (var item in items) // Ska skriva ut allt ovanför 
-      {
-        Console.WriteLine("Name: " + item.name + ", Price: " + item.price); // skriver ut pris och namn på varan
-      }
+    items.Add(new Item() { name = "sofa", price = 1000 }); // skapar en ny item med namn och pris
+    items.Add(new Item() { name = "chair", price = 200 });
+    items.Add(new Item() { name = "lamp", price = 400 });
+    items.Add(new Item() { name = "clock", price = 750 });
+    items.Add(new Item() { name = "table", price = 1200 });
 
+    foreach (var item in items) // Ska skriva ut allt ovanför 
+    {
+      Console.WriteLine("Name: " + item.name + ", Price: " + item.price); // skriver ut pris och namn på varan
     }
 
   }
+
+}
 
 }
