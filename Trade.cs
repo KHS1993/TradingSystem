53f301a46b5c9a9f7fbe41c25a3e5d17e11e11ce
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-04 19:26:46 +0200

    Changed private string username to email as user identifier.

--- Trade.cs
+++ Trade.cs
@@ -6,7 +6,7 @@ public class Trader // Skapar en klass som ska representera en användare (Trade
 
 {
 
-  private string Username { get; set; } // Private på båda då endast klassen själv ska kunna ändra för ökad säkerhet. 
+  private string Email { get; set; } // Private på båda då endast klassen själv ska kunna ändra för ökad säkerhet. 
   private string Password { get; set; } // get och set för att värdet ska kunna ändras men endast inifrån klassen.
 
 }
