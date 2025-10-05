a0e28da5a317d0932749dde31131105257b3e99c
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-05 21:12:40 +0200

    fix(Item): correct constructor, fix typos, add PrintInfo method

--- Item.cs
+++ Item.cs
@@ -2,18 +2,26 @@ using System; //  behövs för console.writeline
 
 namespace MyTradingApp
 {
+  // klass som reprsenterar en vara
   public class Item  // Tillgängligt för andra filer genom att göra klassen public
   {
-    public string Name;
-    public int Price;
+    public string Name { get; set; }
+    public int Price { get; set; }
 
-    public string Description;
+    public string Description { get; set; }
 
+    //Konstruktor för att sapa en ny Item
     public Item(string name, int price, string description) // ska skapa nytt item när dessa tre värden skrivs in.
     {
-      name = name; // Fix: Correct Item constructor to accept name, price, description
+      name = name;
       Price = Price;
       Description = Description;
+    }
+
+    // skriver ut info om varan
+    public void PrintInfo()
+    {
+      Console.WriteLine($"Name: {Name}, Price: {Price}, Description{Description}");
     }
   }
 
