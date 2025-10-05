d886e29a5e6ec732f0395d345cb0f4f0a451f522
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-05 15:53:44 +0200

    chore(TradeStatus): add TradeStatus enum to project namespace

--- Tradestatus.cs
+++ Tradestatus.cs
@@ -1,9 +1,11 @@
 // Pending - Denied - Accepted 
-using System; // Lägger till detta ifall funktionerna behövs från andra filer, vet ej om nödvändigt.
-
-public enum TradeStatus // Ska beskriva vad värdena under, public för att andra klasser ska kunna använda den, tror det behövs på Progran.cs för att se status.
+namespace MyTradingApp
 {
-  Pending,
-  Denied,
-  Accepted,
+  public enum TradeStatus // Ska beskriva vad värdena under, public för att andra klasser ska kunna använda den, tror det behövs på Progran.cs för att se status.
+  {
+    Pending,
+    Denied,
+    Accepted,
+  }
+
 }
