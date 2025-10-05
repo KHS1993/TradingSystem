08f0acf21339fa0a2bb36715bfc8117001b2be7b
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-05 15:53:26 +0200

    fix(Trade): use correct namespace and reference IUser/Role

--- Trade.cs
+++ Trade.cs
@@ -2,15 +2,42 @@
 
 using System;
 
-public class Trader // Skapar en klass som ska representera en användare (Trader). 
-
+namespace MyTradingApp
 {
+  public class Trader : IUser // Skapar en klass som ska representera en användare (Trader). 
 
-  private string Email { get; set; } // Private på båda då endast klassen själv ska kunna ändra för ökad säkerhet. 
-  private string Password { get; set; } // get och set för att värdet ska kunna ändras men endast inifrån klassen.
+  {
+
+    public string Email { get; private set; } // Private på båda då endast klassen själv ska kunna ändra för ökad säkerhet. 
+    private string _password; // get och set för att värdet ska kunna ändras men endast inifrån klassen.
+
+    public Trader(string email, string password)
+    {
+      Email = email;
+      _password = password;
+    }
+
+    public bool TryLogin(string username, string password)
+    {
+      return username == Email && password == _password;
+    }
+
+    public Role GetRole()
+    {
+      return Role.User;
+    }
+
+    public string NewUser()
+    {
+      return "New user created";
+    }
+
+    public bool IsRole(Role role)
+    {
+      return GetRole() == role;
+    }
+  }
 
 }
-
-
 
 
