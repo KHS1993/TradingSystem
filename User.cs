97c3451e24bdd3030b0df46b2f4a917410bb73a5
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-05 22:39:26 +0200

    Rename Trader class, implement Iuser interface, and remove duplicate Iuser class.

--- User.cs
+++ User.cs
@@ -1,77 +1,44 @@
-// email and password
 
 using System.Collections.Generic;
 
 namespace MyTradingApp
 {
-  // En vara som användare kan ladda upp
-  public class Item
-
-  {
-    public string Name { get; set; }
-    public int Price { get; set; }
-    public string Description { get; set; }
-
-    public Item(string name, int price, string description)
-    {
-
-      Name = name;
-      Price = price;
-      Description = description;
-
-    }
-  }
-
-  // Interface för samtliga användare 
+  // Interface för alla användare
   public interface IUser
   {
     bool TryLogin(string email, string password);
     string Email { get; }
-    void UploadItem(Item item); // Ska inte returnera något när anvädaren alddar upp.
+    void UploadItem(Item item);
     List<Item> GetItems();
-
   }
 
-  //Trader är användaren som ska kunna ladda upp varor
+  // Trader är en användare som kan ladda upp varor
   public class Trader : IUser
   {
-
     public string Email { get; private set; }
     private string _password;
     private List<Item> _items;
 
     public Trader(string email, string password)
-
     {
       Email = email;
       _password = password;
       _items = new List<Item>();
-
     }
 
-    // Kontrollera inloggning
     public bool TryLogin(string email, string password)
     {
       return Email == email && _password == password;
-
     }
 
-    // Ladda upp vara
     public void UploadItem(Item item)
-
     {
-
       _items.Add(item);
     }
 
-
-    // Hämta alla varor 
     public List<Item> GetItems()
     {
       return _items;
-
-
     }
   }
 }
-
