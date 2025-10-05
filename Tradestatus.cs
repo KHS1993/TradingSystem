0b50b7f10296013f99dbef6ec0a56c9840dae1af
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-05 22:41:09 +0200

    No major changes.

--- Tradestatus.cs
+++ Tradestatus.cs
@@ -1,6 +1,7 @@
 // Pending - Denied - Accepted 
 namespace MyTradingApp
 {
+  // status för framtida trades
   public enum TradeStatus // Ska beskriva vad värdena under, public för att andra klasser ska kunna använda den, tror det behövs på Progran.cs för att se status.
   {
     Pending,
