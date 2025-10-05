0ce0d019d3fe1d79f95f001a14b87a3c0a7e1bc7
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-05 22:40:14 +0200

    Rename class to TradeHandler, fix constructor name, and correct UploadItem parameter typo.

--- Trade.cs
+++ Trade.cs
@@ -1,17 +1,20 @@
 // requesters - Receiver - status - items 
 
 using System;
+using System.Collections.Generic;
+
+
 
 namespace MyTradingApp
 {
-  public class Trader : IUser // Skapar en klass som ska representera en användare (Trader). 
+  public class TradeHandler // Skapar en klass som ska representera en användare (Trader). 
 
   {
 
     public string Email { get; private set; } // Private på båda då endast klassen själv ska kunna ändra för ökad säkerhet. 
     private string _password; // get och set för att värdet ska kunna ändras men endast inifrån klassen.
-
-    public Trader(string email, string password)
+    private List<Item> _items = new List<Item>();
+    public TradeHandler(string email, string password)
     {
       Email = email;
       _password = password;
@@ -22,6 +25,11 @@ namespace MyTradingApp
       return username == Email && password == _password;
     }
 
+    public enum Role
+    {
+      User,
+      Guest,
+    }
     public Role GetRole()
     {
       return Role.User;
@@ -29,15 +37,34 @@ namespace MyTradingApp
 
     public string NewUser()
     {
-      return "New user created";
+      return $"New user {Email} created";
     }
 
     public bool IsRole(Role role)
     {
       return GetRole() == role;
     }
-  }
 
+
+
+
+    public void UploadItem(string name, int price, string description)
+    {
+      Item newItem = new Item(name, price, description);
+      _items.Add(newItem);
+      Console.WriteLine($"item uploaded: {name}");
+
+
+    }
+    public List<Item> GetItems()
+    {
+      return new List<Item>(_items);
+
+    }
+
+  }
 }
+
+
 
 
