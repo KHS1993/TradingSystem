7e4c8db42d7c30125dd0d7c5f55da762919dcc05
KHS1993 <kifle.selassie@student.nbi-handlesakademin.se>
2025-10-05 22:38:14 +0200

    Fix Item constructor to correctly assign Name, Price, and Description properties.

--- Item.cs
+++ Item.cs
@@ -1,28 +1,24 @@
-using System; //  behövs för console.writeline
+using System;
 
 namespace MyTradingApp
 {
-  // klass som reprsenterar en vara
-  public class Item  // Tillgängligt för andra filer genom att göra klassen public
+  // En vara som användare kan ladda upp
+  public class Item
   {
     public string Name { get; set; }
     public int Price { get; set; }
-
     public string Description { get; set; }
 
-    //Konstruktor för att sapa en ny Item
-    public Item(string name, int price, string description) // ska skapa nytt item när dessa tre värden skrivs in.
+    public Item(string name, int price, string description)
     {
-      name = name;
-      Price = Price;
-      Description = Description;
+      Name = name;
+      Price = price;
+      Description = description;
     }
 
-    // skriver ut info om varan
     public void PrintInfo()
     {
-      Console.WriteLine($"Name: {Name}, Price: {Price}, Description{Description}");
+      Console.WriteLine($"Name: {Name}, Price: {Price}, Description: {Description}");
     }
   }
-
 }
